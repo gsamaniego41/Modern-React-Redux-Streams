9c873b8edd46979eb90dd7b9d29d191d9116712d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-01 11:59:32 -0800

    prints authentication status on screen

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -20,8 +20,18 @@ class GoogleAuth extends React.Component {
     });
   };
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
@@ -29,5 +39,11 @@ export default GoogleAuth;
 
 /* 
 .init() returns a Promise
+window.gapi.getAuthInstance().signIn()
+window.gapi.getAuthInstance().signOut()
 
+Auth Component
+1. Get a reference to the 'auth' object after it is initialized
+2. Figure out if the user is currently signed in
+3. Print their authentication status on screen
 */
