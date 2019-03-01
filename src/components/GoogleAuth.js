4347279a29efd7c690272a69536f677e5647309b
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-01 11:51:14 -0800

    checks gapi if user is signed in

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = {isSignedIn: null};
+  // null bec we don't know whether the user is
+  // signed in or not signed in when our app first loads
+
   componentDidMount = () => {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "1036100617710-l63902et7ppcnemi79gsacb7dane91vu.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "1036100617710-l63902et7ppcnemi79gsacb7dane91vu.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        });
     });
   };
 
@@ -17,3 +26,8 @@ class GoogleAuth extends React.Component {
 }
 
 export default GoogleAuth;
+
+/* 
+.init() returns a Promise
+
+*/
