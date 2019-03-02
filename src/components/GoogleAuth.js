e5a3e0abf23e45dd35b12c1c59649735d073aa76
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-01 21:39:09 -0800

    sign in and sign out helper methods

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -32,19 +32,30 @@ class GoogleAuth extends React.Component {
     this.setState({isSignedIn: this.auth.isSignedIn.get()});
   };
 
+  onSignIn = () => {
+    // we want to access our auth instance
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
-          <i className="google icon">Sign Out</i>
+        <button className="ui red google button" onClick={this.onSignOut}>
+          <i className="google icon" />
+          Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button">
-          <i className="google icon">Sign In</i>
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
+          Sign In with Google
         </button>
       );
     }
