454be13f7496588fafa681c6e38be119f3369ff2
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-03 18:36:45 -0800

    fixed action types to avoid typos

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -72,7 +72,7 @@ class GoogleAuth extends React.Component {
 }
 
 const mapStateToProps = state => {
-  return {isSignedIn: state.isSignedIn};
+  return {isSignedIn: state.auth.isSignedIn};
 };
 
 export default connect(
