20f7e9770a7a3d6ced7d4944bccac30da3a3bcb0
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-04 14:41:10 -0800

    redux-form - creates forms and handles events

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,48 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+// Field and reduxForm are helpers
+// Field is a React component
+// reduxForm is a function, same functionality as connect() function
+class StreamCreate extends React.Component {
+  // renderInput(formProps) {
+  renderInput({input}) {
+    // console.log(formProps);
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+    return (
+      <input
+        /* 
+        type="text"
+        onChange={formProps.input.onChange}
+        value={formProps.input.value} 
+        */
+        // Same as:
+        // {...formProps.input}
+        // Same as:
+        {...input}
+      />
+    );
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        {/* Field is any type of input. name prop required */}
+        {/* 
+          Field is just a component, and not resposible for getting anything on the screen 
+          To tell it what to show, we need a component prop:
+            - either a React component or
+            - a function for the field to call 
+            - They need to return some element that will be shown on he screen
+          It's up to us to show the component and customize it in some fashion
+        */}
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  //unlike connect(), reduxForm receives a single object = configuration
+  form: "streamCreate" // value is what the purpose of the form is
+})(StreamCreate);
