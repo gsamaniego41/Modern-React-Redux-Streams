ac1c46689c0a4c9cd7c8a6f37216cee84da8fbc9
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-05 07:55:01 -0800

    redux-form - displays validation message

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -5,8 +5,9 @@ import {Field, reduxForm} from "redux-form";
 // reduxForm is a function, same functionality as connect() function
 class StreamCreate extends React.Component {
   // renderInput(formProps) {
-  renderInput({input, label}) {
+  renderInput({input, label, meta}) {
     // console.log(formProps);
+    console.log(meta);
 
     return (
       <div className="field">
@@ -22,6 +23,7 @@ class StreamCreate extends React.Component {
           // Same as:
           {...input}
         />
+        <div>{meta.error}</div>
       </div>
     );
   }
@@ -65,22 +67,28 @@ class StreamCreate extends React.Component {
 }
 
 const validate = formValues => {
-  // Doesn't come w/ Redux-Form we have to create it
-  // formValues is going to contain all the diff
-  // values that exist inside of our form
+  //  - Doesn't come w/ Redux-Form we have to create it
+  //  - formValues is going to contain all the diff
+  //    values that exist inside of our form
+  //  - will get called when form is initially rendered OR
+  //    user interacts with it
 
   const errors = {};
   // Did the user enter valid inputs?
   if (!formValues.title) {
     // only run if the user did not enter a title
     errors.title = "You must enter a title";
+    // if the error object has a property w/ the same name we give it
+    // in <Form />, that property + value will be passed to this.renderInput()
   }
   if (!formValues.description) {
     errors.description = "You must enter a description";
   }
+  return errors;
 };
 
 export default reduxForm({
   //unlike connect(), reduxForm receives a single object = configuration
-  form: "streamCreate" // value is what the purpose of the form is
+  form: "streamCreate", // value is what the purpose of the form is
+  validate
 })(StreamCreate);
